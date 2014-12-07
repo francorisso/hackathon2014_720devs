42473aa75c44d6105af7b6e0ca085366f774cb62
mariana stariolo <mariana@720desarrollos.com>
2014-12-06 21:26:44 -0300

    listado

---
+++ design/js/main.js
@@ -0,0 +1,14 @@
+var fancybox={
+	init:function() {
+		$(".fancyvideo").fancybox({
+			fitToView	: false,
+			width		: '100%',
+			height		: '100%',
+			autoSize	: false,
+			closeClick	: false,
+			openEffect	: 'none',
+			padding     : '0',
+			closeEffect	: 'none'
+		});
+	}
+}
