7d86962ba825ea104188201b32776d0552feeb9a
Franco Risso <franco@720desarrollos.com>
2014-12-08 04:55:54 -0300

    test

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,7 +2,7 @@
 
 class BaseController extends Controller {
 
-	protected $google_api_key = "AIzaSyBUsuVB1YULi0Zmmjr4L0hCrDSrpBKzT-U";
+	protected $google_api_key = "AIzaSyBZE4JI_RQWPuK7T3oaJMY3yTkttrducr0";
 	
 	/**
 	 * Setup the layout used by the controller.
