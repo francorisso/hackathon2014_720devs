51b6930a530c045b835d417b080c6f7b6efcee4b
Franco Risso <franco@720desarrollos.com>
2014-12-08 03:14:04 -0300

    updates

--- develop/laravel/app/views/layout/master.blade.php
+++ develop/laravel/app/views/layout/master.blade.php
@@ -6,8 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
 	<meta name="author" content="">
-	<link rel="shortcut icon" href="ico/favicon.ico">
-	<link rel="apple-touch-icon" href="ico/apple-touch-icon.png">
+	<link rel="shortcut icon" href="{{ url('/ico/favicon.png') }}">
+	<link rel="apple-touch-icon" href="{ url('ico/favicon.png') }}">
 
 	<title>Hobbies and Careers</title>
 
